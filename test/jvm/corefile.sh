9f6b7020b3b11d55916bab375261095e6ac54237
kyle11235 <aizuguo_@126.com>
2019-03-04 21:11:50 +0800

    jvm

--- test/jvm/corefile.sh
+++ test/jvm/corefile.sh
@@ -1,6 +1,9 @@
-MAVEN_OPTS="-Xms20M -Xmx20M -XX:+PrintCommandLineFlags -verbose:gc"
+ulimit -c unlimited
+
+export MAVEN_OPTS="-Xms20M -Xmx20M -XX:+PrintCommandLineFlags -verbose:gc"
 
 mvn -f jvm.xml spring-boot:run -Dspring-boot.run.arguments="static,1" 
 
 # kill -6 $PID
+# or ctrl + \
 
--- test/jvm/test.sh
+++
@@ -1,4 +0,0 @@
-MAVEN_OPTS="-Xms20M -Xmx20M -XX:+PrintCommandLineFlags -verbose:gc"
-
-mvn -f jvm.xml spring-boot:run -Dspring-boot.run.arguments="static,10000" 
-
